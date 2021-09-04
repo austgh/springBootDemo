b09c6b26fa4ffb61f0e849e732d25b4e7bcd156e
austgh <46312526+austgh@users.noreply.github.com>
2021-09-04 08:48:23 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # SpringBootDemo
 
-#### 介绍
+## 介绍
 Spring Boot + Mybatis + Mysql 项目demo搭建。
 各种小功能 包括listener intercepter filter 等
-平时功能实现：
+##平时功能实现：
 why 哥 超卖demo 见SellController
 文章链接:https://mp.weixin.qq.com/s/0_5mPhZPdBzb5Gk4ijyQZg
 几行烂代码，我赔了16万。
--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,18 +1,18 @@
 package com.example.demo;
 
 import com.example.demo.util.SpringUtil;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
  * Created by think on 2021/2/18.
  */
-@SpringBootApplication
+//@SpringBootApplication
 //@ServletComponentScan
+@Configuration
 @Import(value={SpringUtil.class})
-public class App1 {
+public class AppConfig {
     @Bean
     public SpringUtil springUtil2() {
         return new SpringUtil();
