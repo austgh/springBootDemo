ecd1e8ed861a552f223b47dcbefc1067ed34271b
austgh <46312526+austgh@users.noreply.github.com>
2023-01-24 10:20:12 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/demo/dao/mapper/UserMapper.java
+++ src/main/java/com/example/demo/dao/mapper/UserMapper.java
@@ -1,9 +1,14 @@
 package com.example.demo.dao.mapper;
 
+import com.example.demo.entity.UserInfo;
+import org.apache.ibatis.annotations.Insert;
+
 /**
  * @author think
  * @version 1.0
  * @date 2021/8/30 15:27
  */
 public interface UserMapper {
+    @Insert("insert into user_info(age,name)values(#{age},#{name})")
+    public void insert(UserInfo userInfo);
 }
