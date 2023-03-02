45524ed44187237b05ed358c9ad94e98dc580666
austgh <1343685859@qq.com>
2023-03-02 21:55:54 +0800

    1

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import com.example.demo.util.SpringUtil;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -11,7 +13,9 @@ import org.springframework.context.annotation.Import;
 //@SpringBootApplication
 //@ServletComponentScan
 @Configuration
+@ComponentScan("com.example.demo")
 @Import(value={SpringUtil.class})
+@MapperScan("com.example.demo.dao.mapper")
 public class AppConfig {
     @Bean
     public SpringUtil springUtil2() {
